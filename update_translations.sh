6d4446b705c102a9bbb14920f7cec3a344767fd8
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-10-13 16:49:53 +1000

    Change translation update to local git

--- update_translations.sh
+++ update_translations.sh
@@ -1,40 +1,32 @@
 #!/bin/sh
 
-for CMD in curl unzip; do
+for CMD in git mv; do
 	if ! command -v "$CMD" >/dev/null 2>&1; then
 		printf "Missing '%s' command\n" "$CMD"
 		exit 1
 	fi
 done
 
-if [ ! -f "$HOME/.weblate" ]; then
-	printf "No Weblate token found\n"
-	exit 1
-fi
+CURR_DIR=$(pwd)
 
-REPO_LANG_DIR="Repo/MustardOS/internal/init/MUOS/language"
-WEBLATE_URL="https://hosted.weblate.org/api/projects/muos/file/"
-TOKEN="$(cat "$HOME/.weblate")"
-FORMAT="zip"
+REPO_LANG_DIR="Repo/MustardOS/language"
+REPO_INTERNAL_DIR="Repo/MustardOS/internal/init/MUOS/language"
 
 tr_rename() {
-	TR_DEST="$HOME/$REPO_LANG_DIR/$2"
-	printf "\033[1mMoving '\033[0m%s\033[1m' to '\033[0m%s\033[1m'\033[0m\n" "$1" "$TR_DEST"
-	mv "$1" "$TR_DEST" || printf "\033[1m\t- Failed on '%s'\033[0m\n" "$1"
+	TR_DEST="$HOME/$REPO_INTERNAL_DIR/$2"
+	printf "\033[1mUpdating '\033[0m%s\033[1m' to '\033[0m%s\033[1m'\033[0m\n" "$1" "$TR_DEST"
+	cp "$1" "$TR_DEST" || printf "\033[1m\t- Failed on '%s'\033[0m\n" "$1"
 }
+
+cd "$HOME/$REPO_LANG_DIR" || (printf "Language repository missing (%s)" "$REPO_LANG_DIR" && exit)
 
 printf "\n=============== \033[1mUpdating Languages\033[0m ==============\n\n"
 printf "\033[1mDownloading Translations\033[0m\n"
 
-if curl -H "Authorization: Token ${TOKEN}" -o "translations.${FORMAT}" "${WEBLATE_URL}?format=${FORMAT}"; then
-	printf "\t\033[1m- Downloaded translations successfully\033[0m\n"
-else
-	printf "\t\033[1m- Failed to download translations\033[0m\n"
-	exit 1
-fi
+git pull
+echo ""
 
-printf "\033[1mDecompressing translations\033[0m\n"
-unzip -j translations.zip
+cd "mux" || (printf "Languages for component 'muX' missing (%s)" "$REPO_LANG_DIR" && exit)
 
 tr_rename "ca.json" "Catalan.json"
 tr_rename "ca@valencia.json" "Valencian.json"
@@ -56,11 +48,11 @@ tr_rename "pt_PT.json" "Portuguese (PT).json"
 tr_rename "ru.json" "Russian.json"
 tr_rename "sv.json" "Swedish.json"
 tr_rename "tr.json" "Turkish.json"
+tr_rename "uk.json" "Ukrainian.json"
 tr_rename "vi.json" "Vietnamese.json"
 tr_rename "zh_Hans.json" "Chinese (Simplified).json"
 tr_rename "zh_Hant.json" "Chinese (Traditional).json"
 
-printf "\033[1mCleaning up!\033[0m\n"
-rm -f translations.zip
+cd "$CURR_DIR" || exit
 
 printf "\n=============== \033[1mLanguages Updated\033[0m ==============\n\n"
