c3663b552bd7132f709c2694958a120e42ab413a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-06 22:54:55 +1000

    Added string support to fnv-1a hash utility

--- utils/fnv1a.c
+++ utils/fnv1a.c
@@ -1,47 +1,79 @@
+#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <errno.h>
 
-static uint32_t fnv1a_hash(FILE *fp) {
+uint32_t fnv1a_hash_str(const char *str) {
+    uint32_t hash = 2166136261U; // FNV offset basis
+
+    for (const char *p = str; *p; p++) {
+        hash ^= (uint8_t) (*p);
+        hash *= 16777619; // FNV prime
+    }
+
+    return hash;
+}
+
+uint32_t fnv1a_hash_file(FILE *file) {
     uint32_t hash = 2166136261U; // FNV offset basis
     unsigned char buf[65535];
     size_t n;
 
-    while ((n = fread(buf, 1, sizeof buf, fp)) > 0) {
+    while ((n = fread(buf, 1, sizeof buf, file)) > 0) {
         for (size_t i = 0; i < n; i++) {
             hash ^= buf[i];
             hash *= 16777619; // FNV prime
         }
     }
 
-    if (ferror(fp)) return 0;
+    if (ferror(file)) return 0;
     return hash;
 }
 
 int main(int argc, char **argv) {
-    FILE *fp = stdin;
-
     if (argc > 2) {
-        fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [file or string]\n", argv[0]);
         return 2;
     }
 
-    if (argc == 2) {
-        fp = fopen(argv[1], "rb");
-        if (!fp) {
-            perror("fopen");
+    if (argc == 1) {
+        errno = 0;
+        uint32_t h = fnv1a_hash_file(stdin);
+
+        if (ferror(stdin)) {
+            perror("fread");
             return 1;
         }
+
+        printf("%08x", h);
+        return 0;
     }
 
-    uint32_t h = fnv1a_hash(fp);
-    if (fp != stdin) fclose(fp);
+    const char *path = argv[1];
 
-    if (h == 0 && errno) {
+    errno = 0;
+    FILE *fp = fopen(path, "rb");
+
+    if (!fp) {
+        if (errno == ENOENT) {
+            uint32_t hash = fnv1a_hash_str(path);
+            printf("%08x", hash);
+            return 0;
+        }
+
+        perror("fopen");
+        return 1;
+    }
+
+    errno = 0;
+    uint32_t hash = fnv1a_hash_file(fp);
+    int had_err = ferror(fp);
+    fclose(fp);
+
+    if (had_err) {
         perror("fread");
         return 1;
     }
 
-    printf("%08x", h);
+    printf("%08x", hash);
     return 0;
 }
